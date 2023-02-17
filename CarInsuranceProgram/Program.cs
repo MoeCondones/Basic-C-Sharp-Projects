57caa7ad9b41b9c58e80ae31f623193c90747012
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2023-02-17 15:46:29 +0000

    new console app for car insurance using boolean logic

---
+++ CarInsuranceProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("have you ever had a DUI violation (yes/no)");
+            string dui = Console.ReadLine();
+
+            Console.WriteLine("how many speeding tickets have you had? ");
+            int SpeedingTickets = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
