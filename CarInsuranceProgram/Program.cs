94e38008647a72bcf09efa7a106101c80dd4dd5f
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2023-02-20 14:26:08 +0000

    ShippingQuotesProgram created to get the estimate price for shipping

--- CarInsuranceProgram/Program.cs
+++ CarInsuranceProgram/Program.cs
@@ -10,22 +10,28 @@ namespace CarInsuranceProgram
     {
         static void Main()
         {
-            Console.WriteLine("What is your age?");
-            int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("What is your age?");
+            //int age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("have you ever had a DUI violation (yes/no)");
-            string dui = Console.ReadLine();
-            bool hasDUI = false;
-            if (dui == "yes")
-            {
-                hasDUI = true;
-            }
+            //Console.WriteLine("have you ever had a DUI violation (yes/no)");
+            //string dui = Console.ReadLine();
+            //bool hasDUI = false;
+            //if (dui == "yes")
+            //{
+            //    hasDUI = true;
+            //}
 
-            Console.WriteLine("how many speeding tickets have you had? ");
-            int SpeedingTickets = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("how many speeding tickets have you had? ");
+            //int SpeedingTickets = Convert.ToInt32(Console.ReadLine());
 
-            bool qualifies = age > 15 && !hasDUI && SpeedingTickets <= 3;
-            Console.WriteLine("qualifies for car insurance: " + qualifies);
+            //bool qualifies = age > 15 && !hasDUI && SpeedingTickets <= 3;
+            //Console.WriteLine("qualifies for car insurance: " + qualifies);
+            //Console.ReadLine();
+
+            int num1 = 8;
+            int num2 = 10;
+            string result = num1 > num2 ? "num1 is greater than num2" : "num 2 is greater than num1";
+            Console.WriteLine(result);
             Console.ReadLine();
 
         }
---
+++ ShippingQuotesProgram/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuotesProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to PAckage Express. Please follow the instructions below");
+            Console.WriteLine("enter package weight: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            else
+            {
+                Console.WriteLine("Enter the package width: ");
+                int width = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the package height");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the package length");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+                int dimensions = width + height + length;
+                if (dimensions > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express");
+                }
+                else
+                {
+                    int AllTogether = width * height * length / 100;
+                    Console.WriteLine("the Price of shipping will be: " + AllTogether + "£");
+                }
+                Console.ReadLine();
+            }
+            
+            
+        }
+    }
+}
