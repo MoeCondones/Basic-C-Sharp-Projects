37dd5def2c0b37331d544d540002e6a85459b0fc
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2023-02-21 12:19:04 +0000

    StringProgram. a project where i work with strings!

---
+++ StringProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Mohamed";
+            int age = 21;
+            string city = "glasgow";
+
+            string concat = "hey my name is " + name + " and im " + age + ", i live in " + city ;
+            string upper = concat.ToUpper();
+            Console.WriteLine(upper);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("this is the stringBuilder assignment");
+            sb.Append(" This is the c# string part of the course");
+            sb.Append(" The sky was painted in shades of orange and pink");
+            sb.Append(" the painting was as bright as the sun");
+
+            string paragraph = sb.ToString();
+            Console.WriteLine(paragraph);
+            Console.ReadLine();
+        }
+    }
+}
