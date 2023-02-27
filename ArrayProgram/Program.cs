f0bf8fbb863adc13bc5789f261761fa5e88f07b9
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2023-02-27 17:33:58 +0000

    fixed a few problems i had with the array.

--- ArrayProgram/Program.cs
+++ ArrayProgram/Program.cs
@@ -10,31 +10,42 @@ namespace ArrayProgram
     {
         static void Main()
         {
-            string[] stringarray = new[] { "pikachu", "evee", "snorlax", };
             Console.WriteLine("pick a number between 0 and 2");
+            string[] stringarray = new[] { "pikachu", "evee", "snorlax", };
 
             int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 2)
+            {
+                Console.WriteLine("this index does not exist. please pick a number between 0 and 2");
+            }
 
             Console.WriteLine("you got " + stringarray[num]);
-            Console.ReadLine();
 
-            int[] intArray = { 5, 123, 234, 324, 13432, 132, 432 };
             Console.WriteLine("enter an index between 0 and 6 to get a special number: ");
+            int[] intArray = { 5, 123, 234, 324, 13432, 132, 432 };
 
             int index = Convert.ToInt32(Console.ReadLine());
+            if (index > 6)
+            {
+                Console.WriteLine("this index does not exist. please pick a number between 0 and 6");
+            }
 
             Console.WriteLine("you got the special number: " + intArray[index]);
-            Console.ReadLine();
-
-            List<string> stringList = new List<string>();
-            stringList.Add("hello");
-            stringList.Add("world");
-            stringList.Add("Aliens");
 
             Console.WriteLine("pick a number between 0 and 2 to get a special string: ");
-            int result = Convert.ToInt32(Console.ReadLine());
+            List<string> stringlist = new List<string>();
+            stringlist.Add("hello");
+            stringlist.Add("world");
+            stringlist.Add("aliens");
 
-            Console.WriteLine("you got the string: " + stringList[result]);
+
+            int result = Convert.ToInt32(Console.ReadLine());
+            if (result > 2)
+            {
+                Console.WriteLine("this index does not exist. please pick a number between 0 and 2");
+            }
+
+            Console.WriteLine("you got the string: " + stringlist[result]);
             Console.ReadLine();
         }
     }
