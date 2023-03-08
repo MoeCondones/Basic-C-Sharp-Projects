0ee490cf37713ed62ad3ee0dac89db22233907f6
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2023-03-08 17:17:00 +0000

    the progress done in the ClassAndMethods console app

---
+++ ClassAndMethod/IntMeth.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethod
+{
+    class IntMeth
+    {
+        public int Operation(int number)
+        {
+            int result = number * 5;
+            return result;
+        }
+        public decimal Operation(decimal num)
+        {
+            int dec = num + 4.52
+            return dec;
+        }
+    }
+}
---
+++ ClassAndMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number: ");
+            int input = int.Parse(Console.ReadLine());
+
+            IntMeth mathOpe = new IntMeth();
+            int result = mathOpe.Operation(input);
+
+            Console.WriteLine("Adding 5 to " + input + " gives " + result);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a number: ");
+            decimal input1 = decimal.Parse(Console.ReadLine());
+
+            int decResult = IntMeth.Operation(input1);
+            Console.WriteLine("the result of the decimal operation:" +decResult);
+            
+            Console.ReadLine();
+        }
+    }
+}
