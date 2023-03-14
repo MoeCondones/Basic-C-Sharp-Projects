7e97c7eb12344245a199438e1eb0f107633e072b
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2023-03-14 00:05:57 +0000

    VoidParameters done!

---
+++ VoidParameters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoidMeth method = new VoidMeth();
+
+            method.voidMeth(4, 20);
+
+            method.voidMeth(num1: 20, num2: 10);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ VoidParameters/VoidMeth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidParameters
+{
+    class VoidMeth
+    {
+        public void voidMeth (int num1, int num2)
+        {
+            int result = num1 + 50;
+
+            Console.WriteLine("resule of the operation: " + result);
+            Console.WriteLine("the value of num2 is: " + num2); 
+        }
+    }
+}
