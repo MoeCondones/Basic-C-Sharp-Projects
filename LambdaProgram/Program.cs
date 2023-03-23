65f2aabae3a1e7f61d62c247eca522cbed2c4f61
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2023-03-23 15:29:30 +0000

    finished the LambdaProgram

--- LambdaProgram/Program.cs
+++ LambdaProgram/Program.cs
@@ -13,15 +13,15 @@ namespace LambdaProgram
             List<Employee> emplyees = new List<Employee>()
             {
                 new Employee {firstName = "Joe", lastName = "Adams", ID = 1 },
-                new Employee {firstName = "Joe", lastName = "alex", ID = 1 },
-                new Employee {firstName = "Joan", lastName = "Dale", ID = 1 },
-                new Employee {firstName = "Joey", lastName = "fridman", ID = 1 },
-                new Employee {firstName = "Jon", lastName = "winehouse", ID = 1 },
-                new Employee {firstName = "Jona", lastName = "dave", ID = 1 },
-                new Employee {firstName = "Jonah", lastName = "bane", ID = 1 },
-                new Employee {firstName = "Jonny", lastName = "bones", ID = 1 },
-                new Employee {firstName = "Jamon", lastName = "barns", ID = 1 },
-                new Employee {firstName = "Jarred", lastName = "lavin", ID = 1 }
+                new Employee {firstName = "Joe", lastName = "alex", ID = 2 },
+                new Employee {firstName = "Joan", lastName = "Dale", ID = 3 },
+                new Employee {firstName = "Joey", lastName = "fridman", ID = 4 },
+                new Employee {firstName = "Jon", lastName = "winehouse", ID = 5 },
+                new Employee {firstName = "Jona", lastName = "dave", ID = 6 },
+                new Employee {firstName = "Jonah", lastName = "bane", ID = 7 },
+                new Employee {firstName = "Jonny", lastName = "bones", ID = 8 },
+                new Employee {firstName = "Jamon", lastName = "barns", ID = 9 },
+                new Employee {firstName = "Jarred", lastName = "lavin", ID = 10 }
             };
             List<Employee> joes = new List<Employee>();
             foreach (Employee employee in emplyees)
@@ -31,7 +31,13 @@ namespace LambdaProgram
                     joes.Add(employee);
                 }
             }
-            Console.WriteLine("there is this many joes: " + joes);
+
+            List<Employee> joes1 = emplyees.Where(x => x.firstName == "Joe").ToList();
+            foreach (Employee joe in joes1)
+            {
+                Console.WriteLine(joe.firstName + " " + joe.lastName + " " + joe.ID);
+            }
+
             Console.ReadLine();
         }
         public class Employee
