07e7c679aa6938a30aeb2ef9344442094cecc876
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2023-03-23 13:09:50 +0000

    StructProgram made to understand structs more!

---
+++ StructProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            Number number2 = number;
+            number2.Amount = 433m / 234m;
+
+            Console.WriteLine("the divided amount is: " + number2.Amount);
+            Console.ReadLine();
+
+        }
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
