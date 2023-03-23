19f1166e511058c6a2c168491aef818759f2c04c
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2023-03-23 15:20:11 +0000

    LambdaProgram in the works

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -10,14 +10,18 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
-            Card card1 = new Card();
-            Card card2 = card1;
-            card1.Face = Face.Eight;
-            card2.Face = Face.King;
+            Deck deck = new Deck();
 
-            Console.WriteLine(card1.Face);
+            //int count = deck.Cards.Count(x => x.Face == Face.Ace);
 
-            //Deck deck = new Deck();
+            //List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList();
+
+            List<int> numberList = new List<int>() { 1, 2, 3, 23, 3, 12, 342, 13, 221, 32 };
+
+            int sum = numberList.Where(x => x > 10).Sum();
+
+            Console.WriteLine(sum);
+
             //deck.Shuffle(3);
 
             //foreach (Card card in deck.Cards)
---
+++ LambdaProgram/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> emplyees = new List<Employee>()
+            {
+                new Employee {firstName = "Joe", lastName = "Adams", ID = 1 },
+                new Employee {firstName = "Joe", lastName = "alex", ID = 1 },
+                new Employee {firstName = "Joan", lastName = "Dale", ID = 1 },
+                new Employee {firstName = "Joey", lastName = "fridman", ID = 1 },
+                new Employee {firstName = "Jon", lastName = "winehouse", ID = 1 },
+                new Employee {firstName = "Jona", lastName = "dave", ID = 1 },
+                new Employee {firstName = "Jonah", lastName = "bane", ID = 1 },
+                new Employee {firstName = "Jonny", lastName = "bones", ID = 1 },
+                new Employee {firstName = "Jamon", lastName = "barns", ID = 1 },
+                new Employee {firstName = "Jarred", lastName = "lavin", ID = 1 }
+            };
+            List<Employee> joes = new List<Employee>();
+            foreach (Employee employee in emplyees)
+            { 
+                if (employee.firstName == "Joe")
+                {
+                    joes.Add(employee);
+                }
+            }
+            Console.WriteLine("there is this many joes: " + joes);
+            Console.ReadLine();
+        }
+        public class Employee
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public int ID { get; set; }
+        }
+    }
+}
